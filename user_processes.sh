627b024b4e490d791dfafd569d9d68e8b9095a2c
Ainur <444biston4@gmail.com>
2026-02-16 16:23:53 +0300

    improve user processes script

--- user_processes.sh
+++ user_processes.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-ps aux | grep ^$USER
+while true
+do
+	read -p "How to sort output? 1) cpu; 2) memory. Type the number: " choice
+	if [ "$choice" == 1 ] || [ "$choice" == 2 ]
+	then
+		break
+	else
+		echo "Wrong input! Try again"
+	fi
+done
+
+if [ "$choice" == 1 ]
+then
+	option="-%cpu"
+elif [ "$choice" == 2 ]
+then
+	option="-%mem"
+fi
+
+ps aux --sort "$option" | grep ^$USER
