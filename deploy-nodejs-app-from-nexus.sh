dfb89de963a58d509ac02a65c39121738b49187f
jamorAA <139582763+jamorAA@users.noreply.github.com>
2026-03-12 16:35:08 +0300

    Add shebang and improve script for artifact download

--- deploy-nodejs-app-from-nexus.sh
+++ deploy-nodejs-app-from-nexus.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -u dev:dev -X GET 'http://158.160.209.188:8081/service/rest/v1/components?repository=npm-snapshots&sort=version' | jq "." > artifact.json
 artifactDownloadUrl=$(jq '.items[].assets[].downloadUrl' artifact.json --raw-output)
 
